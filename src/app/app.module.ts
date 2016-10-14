a8ee202d2fae4472ce17f96b0927a401dd993dd5
Bonnici <nickmarsh@gmail.com>
2016-10-14 21:12:24 +1000

    Added firebase auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AuthProviders, AuthMethods, AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+const myFirebaseConfig = {
+  apiKey: 'AIzaSyAEkS5Heb7Hv0JbmOF9_VlhkCl64FhGyIo',
+  authDomain: 'apartment-price-tracker.firebaseapp.com',
+  databaseURL: 'https://apartment-price-tracker.firebaseio.com',
+  storageBucket: 'apartment-price-tracker.appspot.com',
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Google,
+  method: AuthMethods.Redirect
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +24,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(myFirebaseConfig, myFirebaseAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
