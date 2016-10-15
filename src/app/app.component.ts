1c93393e4802143e0a8b5f9fe5f1c21c66dc8d7a
Bonnici <nickmarsh@gmail.com>
2016-10-15 10:15:21 +1000

    Sample request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFire } from 'angularfire2';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  public title = 'app works!';
+  public response: any;
 
-  constructor(public af: AngularFire) {}
+  constructor(public af: AngularFire, private http: Http) {}
 
-  login() {
+  public ngOnInit() {
+    this.http.get('https://services.realestate.com.au/services/listings/419534970')
+      .subscribe(
+        (response) => this.response = response.json(),
+        (err) => this.response = { err: err });
+
+  }
+
+  public login() {
     this.af.auth.login();
   }
 
-  logout() {
+  public logout() {
     this.af.auth.logout();
   }
 }
