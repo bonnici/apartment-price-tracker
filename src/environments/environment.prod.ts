466559dde43dcb48b5d3e1179f0a156857775d93
Bonnici <nickmarsh@gmail.com>
2017-05-21 16:22:18 +1000

    Added firebase settings to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAEkS5Heb7Hv0JbmOF9_VlhkCl64FhGyIo',
+    authDomain: 'apartment-price-tracker.firebaseapp.com',
+    databaseURL: 'https://apartment-price-tracker.firebaseio.com',
+    storageBucket: 'apartment-price-tracker.appspot.com',
+  }
 };
